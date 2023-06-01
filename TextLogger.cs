389fb57f15cb7c48c6d6f7d2f04e9ef97281ffad
Steven Lim <limhwsteven@gmail.com>
2023-06-01 17:01:21 +0800

    Update TextLogger

--- TextLogger.cs
+++ TextLogger.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks.Dataflow;
 using Microsoft.Extensions.Options;
 
 namespace TradingEngineServer.Logging.LoggingConfiguration
@@ -9,6 +10,31 @@ namespace TradingEngineServer.Logging.LoggingConfiguration
         {
             _loggingConfiguration = loggingConfiguration.Value ?? throw new ArgumentNullException(nameof(loggingConfiguration));
         }
+
+        private static async Task LogAsync(string filepath, BufferBlock<LogInformation> logQueue, CancellationToken token)
+        {
+            using var fs = new FileStream(filepath, FileMode.CreateNew, FileAccess.Write, FileShare.Read);
+            using var sw = new StreamWriter(fs);
+            try
+            {
+                while (true)
+                {
+                    var logItem = await logQueue.ReceiveAsync(token).ConfigureAwait(false);
+                    string formattedMessage = FormatLogItem(logItem);
+                    await sw.WriteAsync(formattedMessage).ConfigureAwait(false);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                
+            }
+        }
+
+        private static string FormatLogItem(LogInformation logItem)
+        {
+            throw new NotImplementedException();
+        }
+
         public void Dispose()
         {
             throw new NotImplementedException();
@@ -16,7 +42,10 @@ namespace TradingEngineServer.Logging.LoggingConfiguration
 
         protected override void Log(LogLevel loglevel, string module, string message)
         {
-            throw new NotImplementedException();
+            _logQueue.Post(new LogInformation(loglevel, module, message, 
+                DateTime.Now, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.Name));
         }
+
+        private readonly BufferBlock<LogInformation> _logQueue = new BufferBlock<LogInformation>();
     }
 }
